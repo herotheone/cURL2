f6d8d5870c3259d6d9052018577db7bbf53e4e31
Emin <eminturk@gmail.com>
2018-09-16 13:44:15 +0300

    Initial Commit

---
+++ cURL2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cURL2
+//
+//  Created by Emin Türk on 16.09.2018.
+//  Copyright © 2018 Emin Türk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
